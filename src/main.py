29ae711b67537703ad50e5d6c885f3d0bb7e3736
tres <tphill760@gmail.com>
2023-11-03 01:05:08 +0000

    fully functioning

--- src/main.py
+++ src/main.py
@@ -1,53 +1,92 @@
-from database.db_connection import execute_query, execute_modify
-
-def main_menu():
-    print('Choose one these options below\n')
-    print('1- Create a hero')
-    input = ('Choose an option below: ')
-    if input == '1':
-        create_hero()
-main_menu()
+from database.db_connection import execute_modify
+from ascii_art import ascii_art
+import os
+from create_hero import select_all_heroes
 
 
+def back_to_main():
+    print('Press Enter to return to Main Menu or press Q to quit! ')
+    prompt = input('').capitalize()
+    if prompt == (''):
+        main_menu()
+    elif prompt == ('Q'):
+        quit()
 
-# Create
+#Create
 def create_hero():
-    name = input('Please enter a name for your hero: ')
-    about = input('Please enter an about-me: ')
-    bio = input('Please enter a short bio: ')
+    print('Please enter a name for your hero:\n ')
+    name = input("")
+    print('Please enter an about-me: ')
+    about = input("")
+    print('Please enter a short bio: ')
+    bio = input("")
     query = """
             INSERT INTO heroes (name, about_me, biography)
             VALUES (%s, %s, %s);
             """
     create = execute_modify(query, (name, about, bio))
+    print("Succesfully added Hero")
+    back_to_main()
 
-
-
-
-
-# Read
-def select_all_heroes():
-    query = """
-        SELECT * from heroes
-    """
-    returned_items = execute_query(query)
-    for item in returned_items:
-        print(item[1])
-    return returned_items
-
-
-
+#Read
+# def select_all_heroes():
+#     query = "SELECT id, name FROM heroes ORDER BY id ASC"
+#     names = execute_query(query)
+#     for count, x in names:
+#         print(f"{count} - {x}")
+#     back_to_main()
 
 # # Update
 def update_hero():
-    name_update = input('Which hero would you like to update? ')
+    select_all_heroes()
+    print('Which hero would you like to update?\n ')
+    option = input('')
+    print('What would you like to change the name to? See list above.\n ')
+    change_name = input('')
     query = """
             UPDATE heroes
             SET name = (%s)
-            WHERE name = new_name; 
+            WHERE id = %s; 
             """
-    new_name = execute_modify(query)
+    new_name = execute_modify(query, (change_name, option,))
+    print(f'Successfully changed hero name to {change_name}')
+    back_to_main()
 
 
-# # Delete
-# def delete_hero()
+# # # Delete
+def delete_hero():
+    select_all_heroes()
+    print('Which hero would you like to delete?\n ')
+    prompt = input('')
+    query = """
+            DELETE FROM heroes 
+            WHERE id = %s
+            """
+    delete_hero = execute_modify(query, (prompt,))
+    print('Successfully deleted hero!')
+    back_to_main()
+
+def main_menu():
+    os.system('clear')
+    ascii_art()
+    print('Choose one these options below\n')
+    print('1- Create a hero')
+    print('2- View all heroes ')
+    print('3- Update a hero ')
+    print('4- Delete hero ')
+    print('Choose an option below: ')
+    option = input("")
+    if option == '1':
+        os.system('clear')
+        create_hero()
+    elif option == '2':
+        os.system('clear')
+        select_all_heroes()
+        back_to_main()
+    elif option == '3':
+        os.system('clear')
+        update_hero()
+    elif option == '4':
+        os.system('clear')
+        delete_hero()
+main_menu()
